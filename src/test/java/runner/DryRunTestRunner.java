77a31f109b155b361a558ddd3de1e82cbf2539c6
Mohamed Nheri <nherimohamed@gmail.com>
2025-03-11 11:31:19 -0400

    Initial commit --- MO

---
+++ src/test/java/runner/DryRunTestRunner.java
@@ -0,0 +1,23 @@
+package runner;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)// You are telling junit to run the TestRunner class as a cucumber Test
+@CucumberOptions(features = "src/test/resources/features/Login_saucedemo.feature",
+                glue = {"stepdefinitions"} ,
+                plugin = {"pretty" , "html:target/primetech-report-2025.html",
+                "json:target/primetech-report-2025.json"},
+                dryRun = true
+
+
+)
+public class TestRunner {
+}
+
+
+/**
+ * This class will allow us to execute and run our feature files/step definitions
+ */
